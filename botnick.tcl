1a386d159a4893eb3689c150f53e325015140549
MadaliN <madalinmen28@gmail.com>
2017-03-20 16:38:07 +0200

    Update botnick.tcl

--- botnick.tcl
+++ botnick.tcl
@@ -1,6 +1,7 @@
-bind PUBM - * botnick
+bind PUBM - * public
+bind MSGM - * private
 
-proc botnick {nick uhost hand chan arg} {
+proc public {nick uhost hand chan arg} {
 	global temp
 
 	if {[string index $arg 0] in {! . `}} {
@@ -10,11 +11,25 @@ proc botnick {nick uhost hand chan arg} {
 	} elseif {[isbotnick [lindex [split $arg] 0]]} {
 		set temp(cmd) [lindex [split $arg] 1]
 		set arg [join [lrange [split $arg] 2 end]]
-	} else { return 0 }
+	} else {
+		set temp(cmd) [lindex [split $arg] 0]
+	}
 
-	if {[info commands pubcmd:$temp(cmd)] ne ""} { pubcmd:$temp(cmd) $nick $uhost $hand $chan $arg }
+	switch -nocase -- $temp(cmd) {
+		o - op - @ {
+			putserv "PRIVMSG $chan :Ok pentru comanda de @ canal"
+		}
+	}
 }
 
-proc pubcmd:help {nick uhost hand chan arg} {
-  putserv "PRIVMSG $chan :Yes.. this is how it works."
+proc private {nick uhost hand arg} {
+	global temp
+
+	set temp(cmd) [lindex [split $arg] 0]
+
+	switch -nocase -- $temp(cmd) {
+		o - op - @ {
+			putserv "PRIVMSG $nick :Ok pentru comanda de @ privat"
+		}
+	}
 }
